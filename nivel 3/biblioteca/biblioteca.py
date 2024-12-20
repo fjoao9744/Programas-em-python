954ba5a2752427567c3d16c2150acec4fcec9390
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2024-12-20 18:32:49 -0300

    Update biblioteca.py

--- nivel 3/biblioteca/biblioteca.py
+++ nivel 3/biblioteca/biblioteca.py
@@ -1,5 +1,5 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QGroupBox, QFrame
 
 
 app = QApplication([])
@@ -16,25 +16,61 @@ class MainPage(QWidget):
         layout = QVBoxLayout()
         
         # Titulo
-        
         title = QLabel("Bem vindo a nossa biblioteca!")
-        
+        title.setAlignment(Qt.AlignmentFlag.AlignCenter)
         title.setStyleSheet("""
             QLabel {
                 font: bold 32px "Fira Sans" ;
 
-
             }
         """)
         
+        master_layout = QVBoxLayout()
+        box = QGroupBox("Oque deseja?")
         
-        layout.addWidget(title, alignment=Qt.AlignmentFlag.AlignCenter | Qt.AlignmentFlag.AlignTop)
+        # Layout 1
+        box_layout1 = QHBoxLayout()
+        book_register = QPushButton("Registrar livro")
+        user_register = QPushButton("Registrar usuario")
+        
+        box_layout1.addWidget(book_register)
+        box_layout1.addWidget(user_register)
+        
+        # Layout 2
+        box_layout2 = QHBoxLayout()
+        book_send = QPushButton("Emprestar Livro")
+        book_back = QPushButton("Devolver livro")
+        
+        box_layout2.addWidget(book_send)
+        box_layout2.addWidget(book_back)
+        
+        # Layout 3
+        box_layout3 = QHBoxLayout()
+        book_display = QPushButton("Emprestar Livro")
+        user_display = QPushButton("Devolver livro")
+        
+        box_layout3.addWidget(book_display)
+        box_layout3.addWidget(user_display)
+        
+        # Adicionando os layouts ao master
+        master_layout.addLayout(box_layout1)
+        master_layout.addLayout(box_layout2)
+        master_layout.addLayout(box_layout3)
+        
+        
+        box.setLayout(master_layout)
+        
+        master_layout.addWidget(box)
+        
+        # Widgets do layout
+        layout.addWidget(title)
+        layout.addWidget(box)
         
         self.setLayout(layout)
         
-
 
 janela_principal = MainPage()
 
 janela_principal.show()
 app.exec_()
+
