a9ac6deff78cac2f6de7e48dbba52d69e213d0b2
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2024-12-20 13:31:47 -0300

    Update biblioteca.py

--- nivel 3/biblioteca/biblioteca.py
+++ nivel 3/biblioteca/biblioteca.py
@@ -1,9 +1,40 @@
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
+
 
 app = QApplication([])
 
-window = QWidget()
+class MainPage(QWidget):
+    def __init__(self) -> None:
+        super().__init__()
+        
+        # Configurações da janela
+        self.setWindowTitle("Biblioteca")
+        self.setGeometry(500, 150, 600, 500)
+        
+        # Layout
+        layout = QVBoxLayout()
+        
+        # Titulo
+        
+        title = QLabel("Bem vindo a nossa biblioteca!")
+        
+        title.setStyleSheet("""
+            QLabel {
+                font: bold 32px "Fira Sans" ;
 
 
-window.show()
+            }
+        """)
+        
+        
+        layout.addWidget(title, alignment=Qt.AlignmentFlag.AlignCenter | Qt.AlignmentFlag.AlignTop)
+        
+        self.setLayout(layout)
+        
+
+
+janela_principal = MainPage()
+
+janela_principal.show()
 app.exec_()
